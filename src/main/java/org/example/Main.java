6534f0d1892833dd7f55988287cdc7182990e0d0
dmptmkn <dm.ptmkn@yandex.ru>
2024-07-17 18:47:54 +0300

    add Main class

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+
+import org.example.printer.CoursePrinter;
+
+public class Main {
+    public static void main(String[] args) {
+        new CoursePrinter(System.out).printData();
+    }
+}
