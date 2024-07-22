75893f0cf66a1979a16c1f6e1ee00fe3dce01d3e
dmptmkn <dm.ptmkn@yandex.ru>
2024-07-22 21:38:41 +0300

    add generic Dao interface

---
+++ src/main/java/org/example/dao/Dao.java
@@ -0,0 +1,13 @@
+package org.example.dao;
+
+import java.util.List;
+
+public interface Dao<K, E> {
+
+    void save(E entity);
+    E findById(K id);
+    List<E> findAll();
+    void update(E entity);
+    void delete(K id);
+
+}
