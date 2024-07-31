3a6b8789e06f44565d70a018d2bfa866b16fc8e4
dmptmkn <dm.ptmkn@yandex.ru>
2024-07-31 18:24:07 +0300

    add test class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,18 @@
 package org.example;
 
-
+import org.example.dao.CourseDaoImpl;
+import org.example.dao.PurchaseDaoImpl;
+import org.example.dao.SubscriptionDaoImpl;
 import org.example.printer.CoursePrinter;
 
 public class Main {
     public static void main(String[] args) {
-        new CoursePrinter(System.out).printData();
+//        new CoursePrinter(System.out).printData();
+//        int designStudentAverageAge = new SubscriptionInfo().getDesignStudentAverageAge();
+//        System.out.println(designStudentAverageAge);
+//
+//        System.out.println(CourseDaoImpl.getInstance().findById(1));
+//        PurchaseDaoImpl.getInstance().findAll().forEach(System.out::println);
+//        SubscriptionDaoImpl.getInstance().findAll().forEach(System.out::println);
     }
 }
